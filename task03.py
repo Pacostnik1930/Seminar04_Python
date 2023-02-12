c05c5d75e14e0571185bc72f206ae279508ef5e1
Петр < >
2023-02-12 18:28:51 +0300

    task03

--- task03.py
+++ task03.py
@@ -2,9 +2,10 @@ n = int(input())
 def simple_number(n):
     flag = True
     for i in range(2,n):
-    #if n // n and n // 1:
-       return flag
-    else :
-        flag = False
-        return flag  
+        if n % i != 0:
+           continue
+        else :
+            flag = False
+            break
+    return flag  
 print(simple_number(n))        
