2e19e8863c50062d1767f4022ce6a6b90158d621
Петр < >
2023-02-12 18:19:17 +0300

    task03

---
+++ task03.py
@@ -0,0 +1,10 @@
+n = int(input())
+def simple_number(n):
+    flag = True
+    for i in range(2,n):
+    #if n // n and n // 1:
+       return flag
+    else :
+        flag = False
+        return flag  
+print(simple_number(n))        
