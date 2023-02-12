4dc328539a21b5ff8fa3bedf6b57400f57d1355e
Петр < >
2023-02-12 17:53:21 +0300

    task02

--- task02.py
+++ task02.py
@@ -2,10 +2,6 @@ import random
 
 
 n = int(input())
-#  def new_list1():
-# list1 = []
-# for i in range(n):
-#     list1.append(random.randint(1,5))
 list1 = [random.randint(1,5)  for i in range(n)]    
 print(list1)
 max = max(list1)
